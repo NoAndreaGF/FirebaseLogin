c762a69798eacfa32bbc570b72a4618f4294a3f1
andrea <andrea.garcia216626@potros.itson.edu.mx>
2022-04-22 12:49:25 -0700

    Initial commit

---
+++ app/src/main/java/garcia/andrea/firebaselogin/MainActivity.kt
@@ -0,0 +1,23 @@
+package garcia.andrea.firebaselogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import garcia.andrea.firebaselogin.databinding.ActivitySignInBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+}
